78e8ad2df3569e28c9eac3a7750f62abe97d68f3
Dat-2210 <ducdat092428@gmail.com>
2025-11-20 22:27:55 +0700

    push lại ProductControllerIntegrationTest.java

--- backend/src/test/java/com/flogin/controller/ProductControllerIntegrationTest.java
+++ backend/src/test/java/com/flogin/controller/ProductControllerIntegrationTest.java
@@ -16,7 +16,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
