551e3ba429c213e2341cf00b73944e0cb2bde279
hoangpixel <hoang.phy.pha.12@gmail.com>
2025-12-05 12:04:43 +0700

    fix CI / CD

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwind()],
-  base: '/FloginFE_BE_Nhom_7/',
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react(), tailwind()],
+    base: '/', // Mặc định: Chạy ở root (cho Test chạy ngon)
+  }
+
+  // Chỉ khi chạy lệnh "build" thì mới đổi đường dẫn cho GitHub Pages
+  if (command === 'build') {
+    config.base = '/FloginFE_BE_Nhom_7/'
+  }
+
+  return config
 })
